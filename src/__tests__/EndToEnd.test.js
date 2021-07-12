6da8a34caca3544896a38742b5cec972b384c60f
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-07-12 23:23:49 +0200

    End-to-End Testing-feature2-Scenario3

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -24,4 +24,10 @@ describe('show/hide an event details', () => {
     const eventDetails = await page.$('.event .event-details');
     expect(eventDetails).toBeDefined();
   });
+
+  test('User can collapse an event to hide its details', async () => {
+    await page.click('.event .details-btn');
+    const eventDetails = await page.$('.event .event-details');
+    expect(eventDetails).toBeNull();
+  });
 });
