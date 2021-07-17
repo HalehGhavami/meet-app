3901b7f186623ef7fdd9e5861c4342e42da5bb1f
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-07-17 10:59:32 +0200

    commented code lines related Google verification-WelcomeScreen

--- src/App.js
+++ src/App.js
@@ -3,90 +3,124 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
+import { getEvents } from './api';
 import { OfflineAlert } from './Alert';
-import WelcomeScreen from './WelcomeScreen';
+// import WelcomeScreen from './WelcomeScreen';
 import './nprogress.css';
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
+    currentLocation: 'all',
     numberOfEvents: 32,
-    offline: false,
-    showWelcomeScreen: undefined,
-  };
-
-  updateEvents = (location, eventCount) => {
-    let locationEvents;
-    getEvents().then((events) => {
-      if (location === 'all' && eventCount === 0) {
-        locationEvents = events;
-      } else if (location !== 'all' && eventCount === 0) {
-        locationEvents = events.filter((event) => event.location === location);
-      } else if (location === '' && eventCount > 0) {
-        locationEvents = events.slice(0, eventCount);
-      } else if (location === '' && eventCount === '') {
-        locationEvents = events;
-      }
-      this.setState({
-        events: locationEvents,
-        numberOfEvents: eventCount,
-        offline: navigator.onLine ? false : true,
-      });
-    });
+    warningText: '',
+    // offline: false,
+    // showWelcomeScreen: undefined,
   };
 
   async componentDidMount() {
     this.mounted = true;
-    const accessToken = localStorage.getItem('access_token');
-    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = searchParams.get('code');
-    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    if ((code || isTokenValid) && this.mounted) {
-      getEvents().then((events) => {
-        if (this.mounted) {
-          this.setState({
-            events: events.slice(0, this.state.numberOfEvents),
-            locations: extractLocations(events),
-            offline: navigator.onLine ? false : true,
-          });
-        }
+    // const accessToken = localStorage.getItem('access_token');
+    // const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    // const searchParams = new URLSearchParams(window.location.search);
+    // const code = searchParams.get('code');
+    // this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    // if ((code || isTokenValid) && this.mounted) {
+    //   getEvents().then((events) => {
+    //     if (this.mounted) {
+    //       this.setState({
+    //         events: events.slice(0, this.state.numberOfEvents),
+    //         locations: extractLocations(events),
+    //         offline: navigator.onLine ? false : true,
+    //       });
+    //     }
+    //   });
+
+    if (!navigator.onLine) {
+      this.setState({
+        warningText:
+          'You are currently using the app offline and viewing data from your last visit. Data will not be up-to-date.',
       });
+    } else {
+      this.setState({ warningText: '' });
+    }
+    if (this.mounted) {
+      this.updateEvents();
     }
   }
 
   componentWillUnmount() {
     this.mounted = false;
-    window.removeEventListener('offline');
   }
+
+  // Filters events based on location and number given in user input
+  updateEvents = (location, eventCount) => {
+    const { currentLocation, numberOfEvents } = this.state;
+
+    // If user selects a location from input
+    if (location) {
+      getEvents().then((response) => {
+        // Applies new filter for location
+        const locationEvents =
+          location === 'all'
+            ? response.events
+            : response.events.filter((event) => event.location === location);
+        const events = locationEvents.slice(0, numberOfEvents);
+        return this.setState({
+          events: events,
+          currentLocation: location,
+          locations: response.locations,
+        });
+      });
+    } else {
+      getEvents().then((response) => {
+        // Persists location filter from state
+        const locationEvents =
+          currentLocation === 'all'
+            ? response.events
+            : response.events.filter(
+                (event) => event.location === currentLocation
+              );
+        const numEvents = eventCount || numberOfEvents;
+        const events = locationEvents.slice(0, numEvents);
+        if (this.mounted) {
+          return this.setState({
+            events: events,
+            numberOfEvents: eventCount,
+            locations: response.locations,
+          });
+        }
+      });
+    }
+  };
+
   render() {
-    if (this.state.showWelcomeScreen === undefined)
-      return <div className="App" />;
+    const { numberOfEvents, events, locations, warningText } = this.state;
+    // if (this.state.showWelcomeScreen === undefined)
+    // return <div className="App" />;
     return (
       <div className="App">
-        {this.state.offline && (
-          <OfflineAlert text="You are offline! The shown events have been loaded from the cache" />
-        )}
+        <OfflineAlert text={warningText} />
         <h1>Meet App</h1>
         <h4>Choose your nearest city</h4>
         <CitySearch
-          locations={this.state.locations}
+          locations={locations}
           updateEvents={this.updateEvents}
-          numberOfEvents={this.state.numberOfEvents}
+          // numberOfEvents={this.state.numberOfEvents}
         />
         <NumberOfEvents
-          numberOfEvents={this.state.numberOfEvents}
+          numberOfEvents={numberOfEvents}
           updateEvents={this.updateEvents}
         />
-        <EventList events={this.state.events} />
-        <WelcomeScreen
+
+        <EventList events={events} />
+        {/* <WelcomeScreen
           showWelcomeScreen={this.state.showWelcomeScreen}
           getAccessToken={() => {
             getAccessToken();
           }}
-        />
+        /> */}
       </div>
     );
   }
